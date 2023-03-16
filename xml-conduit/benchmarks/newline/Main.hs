79103ca80d6f9280138db01a98cc6164115b5b78
Paul Brinkmeier <paul.brinkmeier@channable.com>
2023-03-16 16:56:47 +0100

    Add benchmark

---
+++ xml-conduit/benchmarks/newline/Main.hs
@@ -0,0 +1,27 @@
+module Main (main) where
+
+import Conduit (runConduitRes, (.|))
+import Control.DeepSeq (force)
+import Control.Monad.IO.Class (liftIO)
+import Control.Exception (evaluate)
+import Criterion.Main (defaultMain, bench, whnfIO)
+import Data.Conduit.Binary (sourceFile)
+
+import qualified Data.Conduit.Combinators as CC
+
+import Text.XML.Stream.Parse (parseBytes, def)
+
+main :: IO ()
+main = do
+  defaultMain
+    [ bench "with newline normalization" $ whnfIO (parse "./big.xml")
+    , bench "with newline normalization, no carriage returns" $ whnfIO (parse "./big-nocr.xml")
+    ]
+
+  where
+    parse :: FilePath -> IO ()
+    parse path = runConduitRes $
+      sourceFile path
+      .| parseBytes def
+      .| CC.mapM (liftIO . evaluate . force)
+      .| CC.sinkNull
